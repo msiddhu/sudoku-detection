66b4c023b25403a13d0cdfbaf070bbfc479aa3fb
msiddhu <msiddhu45@gmail.com>
2020-09-29 05:12:28 +0530

    third commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import numpy as np
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
